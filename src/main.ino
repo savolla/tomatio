4709081f76c5a5370bad2ce13a47616a3e9c5652
savolla <savolla@protonmail.com>
2021-04-20 18:47:42 +0300

    can display minutes from 0 to 99

--- src/main.ino
+++ src/main.ino
@@ -1,33 +1,40 @@
+#define SHIFTR_CLOCK2        (6)
+#define SHIFTR_DATA2         (5)
 #define SHIFTR_CLOCK         (4)
 #define SHIFTR_LATCH         (3)
 #define SHIFTR_DATA          (2)
 #define SHIFTR_CLEAR         (1)
 #define SHIFTR_OUTPUT_ENABLE (0)
 
-const char encodedSevenSegmentNumbers[10] = {
-/* 0 */ 64,
-/* 1 */ 121,
-/* 2 */ 36,
-/* 3 */ 48,
-/* 4 */ 25,
-/* 5 */ 18,
-/* 6 */ 3,
-/* 7 */ 120,
-/* 8 */ 128,
-/* 9 */ 24
+const unsigned char encodedSevenSegmentNumbers[10] = {
+/* 0 */ 0b11111100,
+/* 1 */ 0b01100000,
+/* 2 */ 0b11011010,
+/* 3 */ 0b11110010,
+/* 4 */ 0b01100110,
+/* 5 */ 0b10110110,
+/* 6 */ 0b00111110,
+/* 7 */ 0b11100000,
+/* 8 */ 0b11111110,
+/* 9 */ 0b11100110
 };
 
 // function declarations
-void sevenSegmentShowNumber( const char );
+void sevenSegmentShowNumber( const char, const char, const char );
 void clearPreviousNumber( void );
 void updateSevenSegment( void );
+void displayMinute( const char );
 
 // runtime
 void setup(void) {
-// setting pin modes
+  // setting pin modes
+
   pinMode(SHIFTR_CLOCK,         OUTPUT);
-  pinMode(SHIFTR_LATCH,         OUTPUT);
+  pinMode(SHIFTR_CLOCK2,        OUTPUT);
   pinMode(SHIFTR_DATA,          OUTPUT);
+  pinMode(SHIFTR_DATA2,         OUTPUT);
+
+  pinMode(SHIFTR_LATCH,         OUTPUT);
   pinMode(SHIFTR_CLEAR,         OUTPUT);
   pinMode(SHIFTR_OUTPUT_ENABLE, OUTPUT);
 
@@ -36,18 +43,28 @@ void setup(void) {
 }
 
 void loop(void) {
-        for ( char i = 0; i < 10; i++ ) {
-        sevenSegmentShowNumber( i );
+        for ( char i = 0; i < 100; i++ ) {
+        displayMinute( i );
         delay(1000);
     }
 }
 
 // function definitions
-void sevenSegmentShowNumber( const char number ) {
-    const char encodedNumber = encodedSevenSegmentNumbers[ number ];
+void displayMinute( const char minute ) {
+    const char onesDigit = minute%10;
+    const char tensDigit = minute/10;
     clearPreviousNumber(); // from Seven Segment
-    shiftOut(SHIFTR_DATA, SHIFTR_CLOCK, MSBFIRST, encodedNumber);
+    sevenSegmentShowNumber( onesDigit, SHIFTR_DATA2, SHIFTR_CLOCK2 );
+    sevenSegmentShowNumber( tensDigit, SHIFTR_DATA, SHIFTR_CLOCK );
     updateSevenSegment();
+}
+
+
+void sevenSegmentShowNumber( const char number,
+                             const char serialDataPin,
+                             const char serialClockPin ) {
+    const char encodedNumber = encodedSevenSegmentNumbers[number];
+    shiftOut(serialDataPin, serialClockPin, LSBFIRST, encodedNumber);
 }
 
 void clearPreviousNumber( void ) {
@@ -56,7 +73,7 @@ void clearPreviousNumber( void ) {
    digitalWrite(SHIFTR_CLEAR, HIGH);
 }
 
-void updateSevenSegment() {
+void updateSevenSegment( void ) {
     digitalWrite(SHIFTR_LATCH, HIGH);
     digitalWrite(SHIFTR_LATCH, LOW);
 }
